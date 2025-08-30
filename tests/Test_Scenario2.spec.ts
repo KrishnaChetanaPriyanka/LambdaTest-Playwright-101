8af33f2adc9a8639593d0f7870aee84cc2d6cbf8
Krishna Chetana Priyanka Perisetty <chetanpriyanka1826@gmail.com>
2025-08-30 23:59:29 +0530

    Update Test_Scenario2.spec.ts

--- tests/Test_Scenario2.spec.ts
+++ tests/Test_Scenario2.spec.ts
@@ -2,10 +2,10 @@ import { test, expect } from '@playwright/test';
 
 test('Drag & Drop Slider to 95', async ({ page }) => {
   await page.goto('https://www.lambdatest.com/selenium-playground');
-await page.getByRole('link', { name: 'Drag & Drop Sliders' }).click();
-await page.getByRole('heading', { name: 'Default value 15' }).click();
-await page.locator('#slider3').getByRole('slider').fill('95');
-await page.getByText('95').click();
+  await page.getByRole('link', { name: 'Drag & Drop Sliders' }).click();
+  await page.getByRole('heading', { name: 'Default value 15' }).click();
+  await page.locator('#slider3').getByRole('slider').fill('95');
+  await page.getByText('95').click();
 
 
   /*await page.getByRole('link', { name: 'Drag & Drop Sliders' }).click();
